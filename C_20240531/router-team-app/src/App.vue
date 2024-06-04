75433c223f19c161c6e1227c5ed824d2bb0c155e
M6F\student <qhdbs0802@naver.com>
2024-06-04 17:38:01 +0900

    Front-End

---
+++ C_20240531/router-team-app/src/App.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="container">
+    <Header />
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header.vue";
+import { provide } from "vue";
+
+export default {
+  name: "App",
+  components: { Header },
+  setup() {
+    provide("videos", [
+      {
+        id: "ft70sAYrFyY",
+        title: "Bubble Gum Official MV",
+        category: "official",
+      },
+      {
+        id: "Q3K0TOvTOno",
+        title: "How Sweet Official MV",
+        category: "official",
+      },
+      {
+        id: "js1CtxSY38I",
+        title: "Attention Official MV",
+        category: "official",
+      },
+      { id: "jOTfBlKSQYY", title: "ETA Official MV", category: "official" },
+      {
+        id: "kKsivrgoyDw",
+        title: "Cool With You Official MV",
+        category: "official",
+      },
+      {
+        id: "ArmDp-zijuc",
+        title: "Super Shy Official MV",
+        category: "official",
+      },
+      {
+        id: "9wUKhEgnllc",
+        title: "Hype boy Official MV",
+        category: "official",
+      },
+      {
+        id: "VOmIplFAGeg",
+        title: "Cookie Official MV",
+        category: "official",
+      },
+      {
+        id: "tVIXY14aJms",
+        title: "Hurt Official MV",
+        category: "official",
+      },
+    ]);
+  },
+};
+</script>
+<style>
+@import url("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css");
+.container {
+  text-align: center;
+  margin-top: 10px;
+}
+.fa {
+  padding: 5px;
+  width: 30px;
+  text-align: center;
+  text-decoration: none;
+  margin: 5px 2px;
+}
+.fa-facebook {
+  background: #3b5998;
+  color: white;
+}
+.fa-youtube {
+  background: #bb0000;
+  color: white;
+}
+.fa-instagram {
+  background: #125688;
+  color: white;
+}
+</style>
---
+++ C_20240531/router-team-app/src/components/Header.vue
@@ -0,0 +1,53 @@
+<template>
+  <nav class="navbar navbar-expand-md bg-dark navbar-dark mt-2 header">
+    <span class="navbar-brand" style="width: 200px">뉴진스(NewJeans)</span>
+    <button class="navbar-toggler" type="button" @click="changeIsNavShow">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div :class="navClass">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <router-link class="nav-link" to="/">홈</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/about">소개</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/members">멤버</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/videos">영상</router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+import { reactive, computed } from "vue";
+
+export default {
+  setup() {
+    const state = reactive({
+      isNavShow: false,
+    });
+    const navClass = computed(() =>
+      state.isNavShow
+        ? "collapse navbar-collapse show"
+        : "collapse navbar-collapse"
+    );
+    const changeIsNavShow = () => {
+      state.isNavShow = !state.isNavShow;
+    };
+
+    return { state, changeIsNavShow, navClass };
+  },
+};
+</script>
+
+<style>
+.header {
+  background-color: rgb(126, 59, 148) !important;
+}
+</style>

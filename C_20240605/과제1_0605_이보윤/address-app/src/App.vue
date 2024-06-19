5e3a1bb48c6799a77c9b1b84f390506de69388ae
M6F\student <qhdbs0802@naver.com>
2024-06-19 10:30:34 +0900

    Front-End

--- C_20240605/과제1_0605_이보윤/address-app/src/App.vue
+++ C_20240605/과제1_0605_이보윤/address-app/src/App.vue
@@ -1,28 +1,13 @@
 <template>
   <div id="app">
-    <router-view />
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import { createRouter, createWebHistory } from "vue-router";
-import MainPage from "@/components/MainPage.vue";
-import CreatePage from "@/components/CreatePage.vue";
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    { path: "/", name: "MainPage", component: MainPage },
-    { path: "/create", name: "CreatePage", component: CreatePage },
-  ],
-});
-
 export default {
   name: "App",
-  router,
 };
 </script>
 
-<style>
-/* 스타일 작성 */
-</style>
+<style lang="scss" scoped></style>
